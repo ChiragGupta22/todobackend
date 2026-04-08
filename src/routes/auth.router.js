a12381c44f8cf5fba40e0053b877412255f04495
Chirag <chiragguptaworks@gmail.com>
2026-04-08 22:01:52 +0530

    yes done

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const authController = require("../controller/user.controller");
-const authMiddleware = require("../middleware/auth.middleware");
+// const authMiddleware = require("../middleware/auth.middleware");
 const router = express.Router();
 
 router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
 router.post("/logout", authController.logoutUser);
-router.get("/me", authMiddleware, authController.getMe);
+router.get("/me", authController.getMe);
 
 module.exports = router;
